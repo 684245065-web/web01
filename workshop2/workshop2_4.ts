706577406c554a2915d3be52a7c57a39817b468e
684245065-web <684245065@webmail.npru.ac.th>
2026-06-24 13:05:46 +0700

    Update workshop2_4.ts

--- workshop2/workshop2_4.ts
+++ workshop2/workshop2_4.ts
@@ -4,9 +4,9 @@ let TotalScore: number = 0;
 
 console.log(`Score: ${score}`);
 console.log(`Student ID: ${std_Data[0]}, Name: ${std_Data[1]}`);
-for (let i = 0; i < scores.legth; i++){
+for (let i = 0; i < score.length; i++){
     console.log(`Test: ${i + 1} score: ${score[i]}`);
-    TotalScore += scores[i];
+    TotalScore += score[i];
 }
-console.log(`Total Score: ${TtalScore}`);
+console.log(`Total Score: ${TotalScore}`);
 console.log(`Average Score: ${TotalScore / score.length}`);
