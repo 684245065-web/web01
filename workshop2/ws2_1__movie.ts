d8d885d616278f78ae1a957627ecebd6a477435c
684245065-web <684245065@webmail.npru.ac.th>
2026-06-24 13:06:58 +0700

    Update ws2_1__movie.ts

--- workshop2/ws2_1__movie.ts
+++ workshop2/ws2_1__movie.ts
@@ -2,7 +2,7 @@ let age: number = 9;
 console.log("Age ",age);
 if (age<12){
     console.log("Movie Price: $5");
-}else if(age<=60);{
+}else if(age<=60){
     console.log("Movie Price: $10");
-}else
-    console.log("Moive Price: $7");
+}else{
+    console.log("Moive Price: $7");}
