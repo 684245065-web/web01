e9bd340141de8ed5d8012a5dacba47ec33ca7a17
684245065-web <684245065@webmail.npru.ac.th>
2026-06-24 13:05:00 +0700

    Update workshop2_5.ts

--- workshop2/workshop2_5.ts
+++ workshop2/workshop2_5.ts
@@ -1,11 +1,11 @@
-let store: [product: string, price: number, quantity: number]] = [["GPU", 25000, 5], ["Smartphon"];
+let store: [product: string, price: number, quantity: number][] = [["GPU", 25000, 5], ["Smartphone",15000, 10]];
 for(let [product, price, quantity] of store) {
-    console.log(` Product: ${product}, Price: ${price}, Quantity: ${Squantity}`);
+    console.log(` Product: ${product}, Price: ${price}, Quantity: ${quantity}`);
 }
 store. push(["Laptop", 20000, 8]);
 store. push (["Mouse", 2400, 12]);
-store. push (["Keyboard"1 3500, 20]);
+store. push (["Keyboard", 3500, 20]);
 console.log ("\nUpdated store inventory:");
 for (let [product, price, quantity] of store) {
-    console.log(`Product: $(product), price: $(price), Quantity: $(quantity)`);
+    console.log(`Product: ${product}, price: ${price}, Quantity: ${quantity}`);
 }
